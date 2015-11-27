650021630961eb32b08050a661eec95f81a28aa4
Sourabh Dev <sourabh.dev90@gmail.com>
2015-11-27 09:18:19 +0530

    chore: got the flask server running on port 5000

--- project.py
+++ project.py
@@ -1,2 +1,11 @@
 __author__ = 'Sourabh Dev'
 
+from flask import Flask, render_template, request, redirect, url_for
+from flask import sessions, jsonify, flash
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5000)
