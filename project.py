dd559a78771fb2829fbd24ea50671686d0715bf1
Sourabh Dev <sourabh.dev90@gmail.com>
2015-11-27 09:21:19 +0530

    feat: added index page

--- project.py
+++ project.py
@@ -6,6 +6,13 @@ from flask import sessions, jsonify, flash
 
 app = Flask(__name__)
 
+
+# Home Page for the application
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(port=5000)
