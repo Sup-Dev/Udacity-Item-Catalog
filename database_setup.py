1912593fd57334d54ad546a28d120ec1acbe7ae1
Sourabh Dev <sourabh.dev90@gmail.com>
2015-11-28 09:32:31 +0530

    feat: added database setup and populate scripts

---
+++ database_setup.py
@@ -0,0 +1,31 @@
+__author__ = 'Sourabh Dev'
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(500))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+
+Base.metadata.create_all(engine)
---
+++ populate_database.py
@@ -0,0 +1,58 @@
+__author__ = 'Sourabh Dev'
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Category, Item
+
+engine = create_engine('sqlite:///itemcatalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Items for Football
+category1 = Category(name="Football")
+
+session.add(category1)
+session.commit()
+
+item1 = Item(title="Football", description="A black and white ball", category=category1)
+
+session.add(item1)
+session.commit()
+
+
+# Items for Cricket
+category2 = Category(name="Cricket")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(title="Bat", description="A wooden bat to strike the ball with", category=category2)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(title="Ball", description="A red coloured ball made of cork", category=category2)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(title="Wicket", description="A wicket made out of wood", category=category2)
+
+session.add(item3)
+session.commit()
+
+
+print("added menu items!")
