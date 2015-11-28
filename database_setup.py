e9a39aee309560654f2c1e806e36e0c0b707f72a
Sourabh Dev <sourabh.dev90@gmail.com>
2015-11-28 10:11:32 +0530

    fix: added created_date to Item table

--- database_setup.py
+++ database_setup.py
@@ -1,9 +1,10 @@
 __author__ = 'Sourabh Dev'
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -21,6 +22,7 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(80), nullable=False)
     description = Column(String(500))
+    created_date = Column(DateTime(timezone=True), default=func.now())
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
