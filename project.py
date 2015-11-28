78f26016f2e9c83e2948eee3a77161c6ba7e0e47
Sourabh Dev <sourabh.dev90@gmail.com>
2015-11-28 13:32:53 +0530

    feat: add page to view items in a category

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from flask import sessions, jsonify, flash
 
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
 from database_setup import Base, Category, Item
 
@@ -24,6 +25,17 @@ session = DBSession()
 def index():
     items = session.query(Item).order_by(desc(Item.created_date))
     return render_template('index.html', categories=get_all_categories(), items=items)
+
+
+# Page shows items in a category
+@app.route('/<category>/items')
+def category_items(category):
+    try:
+        category = session.query(Category).filter_by(name=category).one()
+        items = session.query(Item).filter_by(category=category)
+        return render_template('category_items.html', categories=get_all_categories(), items=items)
+    except NoResultFound:
+        return redirect(url_for('index'))
 
 
 # Helper functions
